63e12a816e77b43b16e7689c35250773e8ae95a1
Davide Concu <davideconcu@Davides-MacBook-Pro.local>
2020-09-02 12:50:12 +0900

    Walk forward validation data split finished

--- DataPipeline/SequenceGenerator/SequenceGen.py
+++ DataPipeline/SequenceGenerator/SequenceGen.py
@@ -4,13 +4,13 @@ import torch
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 
-class dataSequenceGenerator():
-    def __init__(self, sequenceLength, targetLength, targetFeatures:list,splitRatio,dataScaling:str):
-        self.dfFolderPath = '../WebScraping/MergedTables'
-        self.symbolsTable = '../WebScraping/docs/Symbols.csv'
-        self.discardedCompaniesList = '/docs/discardedList.txt'
-        self.trainingOnlyList = '/docs/trainingOnlyList.txt'
-        self.testList = '/docs/TestList.txt'
+class SequenceBuilder():
+    def __init__(self, dfFolderPath, sequenceLength, targetLength, targetFeatures:list,splitRatio,dataScaling:str):
+        self.ROOT_DIR = '/Users/davideconcu/Documents/Stock Analysis'
+        self.dfFolderPath = dfFolderPath
+        self.discardedCompaniesList = self.ROOT_DIR + '/DataPipeline/SequenceGenerator/docs/discardedList.txt'
+        self.trainingOnlyList = self.ROOT_DIR + '/DataPipeline/SequenceGenerator/docs/trainingOnlyList.txt'
+        self.testList = self.ROOT_DIR + '/DataPipeline/SequenceGenerator/docs/TestList.txt'
         self.sequenceLength = sequenceLength
         self.targets = targetFeatures
         self.numTargets = len(targetFeatures)
@@ -20,31 +20,30 @@ class dataSequenceGenerator():
         self.dataScaling = dataScaling
         self.scaler = None
 
-    def WalkForwardSplit(self,splitRatio:int):
+    def WalkForwardSplit(self):
         hashDfTrain = {}
         hashDfTest = {} 
-        for file in os.listdir(self.dfFolderPath):
+        for file in os.listdir(self.ROOT_DIR+self.dfFolderPath):
             if file.endswith(".csv"):
-                df = pd.read_csv(file,index_col=[0])
+                df = pd.read_csv(self.ROOT_DIR+self.dfFolderPath+file,index_col=[0])
                 symbolName = file.split('_')[0]
                 dfLength = len(df)
                 if len(df) < self.sequenceLength + self.targetLength:
-                    self.__writeListFile(self.discardedCompaniesList)
+                    self.__writeListFile(self.discardedCompaniesList,symbolName)
                 elif len(df) < self.sequenceLength + 3*self.targetLength:
-                    self.__writeListFile(self.trainingOnlyList)
-                    splitIndex = self.__findSplitIndex(dfLength)
-                    hashDfTrain[symbolName] = df[:splitIndex]
+                    self.__writeListFile(self.trainingOnlyList,symbolName)
+                    hashDfTrain[symbolName] = df
                 else:
-                    self.__writeListFile(self.testList)
+                    self.__writeListFile(self.testList,symbolName)
                     splitIndex = self.__findSplitIndex(dfLength)
                     hashDfTrain[symbolName] = df[:splitIndex]
                     hashDfTest[symbolName] = df[(splitIndex-self.sequenceLength):]
-        self.__getLabels(hashDfTrain[hashDfTrain.keys([0])]) # get features labels
+        self.__getLabels(hashDfTrain[list(hashDfTrain.keys())[0]]) # get features labels
         self.scaler = self.__computeStandardization(hashDfTrain) # compute scaler over all training data
-        x_train_wf, y_train_wf = self.__multivariateDataWFsplitWrapper(hashDfTrain,0)
+        x_train_wf, y_train_wf = self.__multivariateDataWFsplitWrapper(hashDfTrain,0,self.scaler)
         assert x_train_wf[-1].shape == torch.Size([self.sequenceLength, len(self.inputFeatures)]), 'Wrong Train Input shape'
         assert y_train_wf[-1].shape == torch.Size([self.targetLength, self.numTargets]), 'Wrong Train Target shape'
-        x_validation_wf, y_validation_wf = self.__multivariateDataWFsplitWrapper(hashDfTrain,self.targetLength)
+        x_validation_wf, y_validation_wf = self.__multivariateDataWFsplitWrapper(hashDfTrain,self.targetLength,self.scaler)
         assert x_validation_wf[-1].shape == torch.Size([self.sequenceLength, len(self.inputFeatures)]), 'Wrong Validation Input shape'
         assert y_validation_wf[-1].shape == torch.Size([self.targetLength, self.numTargets]), 'Wrong Validation Target shape'
         return x_train_wf, y_train_wf, x_validation_wf, y_validation_wf, self.scaler , hashDfTest
@@ -59,42 +58,43 @@ class dataSequenceGenerator():
         x_scaled = scaler.fit(x)
         return scaler
     
-    def __multivariateDataWFsplitWrapper(self,elementToSplit,startIdx):
+    def __multivariateDataWFsplitWrapper(self,elementToSplit,startIdx,scaler):
+        if isinstance(elementToSplit,dict):
+            inputData,targetData = [],[]
+            for k in elementToSplit.keys():
+                inputDataK,targetDataK = self.multivariateDataWFsplit(elementToSplit[k],startIdx, 
+                                                None, self.sequenceLength, self.targetLength, 1,scaler)
+                inputData += inputDataK
+                targetData += targetDataK
+        elif isinstance(elementToSplit,pd.DataFrame):
+            inputData,targetData = self.multivariateDataWFsplit(elementToSplit[k],startIdx, 
+                                                None, self.sequenceLength, self.targetLength, 1,scaler)
+        return torch.tensor(inputData).reshape(-1,self.sequenceLength,len(self.inputFeatures)), torch.tensor(targetData).reshape(-1,self.targetLength,self.numTargets)
+    
+    def multivariateDataWFsplit(self,df,startIndex, end_index, historySize, targetSize, step,scaler):
         data = []
         labels = []
-        if isinstance(elementToSplit,dict):
-            for k in elementToSplit.keys():
-                inputData,targetData = self.multivariateDataWFsplit(self,k,startIdx, 
-                                                None, self.sequenceLength, self.targetLength, 1)
-                data.append(inputData)
-                labels.append(targetData)
-        elif isinstance(elementToSplit,pd.DataFrame):
-            inputData,targetData = self.multivariateDataWFsplit(self,k,startIdx, 
-                                                None, self.sequenceLength, self.targetLength, 1)
-            data.append(inputData)
-            labels.append(targetData)
-        return torch.tensor(data), torch.tensor(labels).reshape(-1,self.targetLength,self.numTargets)
-    
-    def multivariateDataWFsplit(self,df,startIndex, end_index, historySize, targetSize, step):
         dataset = df[self.inputFeatures].values
-        dfScaled = scaler.transform(df)
+        dfScaled = scaler.transform(dataset)
         dataset_target = dfScaled[:,0:self.numTargets]
         startIndex = startIndex + historySize
         if end_index is None:
             end_index = len(dataset) - targetSize
-        for i in range(startIndex, end_index):
+        for i in range(startIndex, end_index+1,2*targetSize):
             indices = range(i-historySize, i, step)
-        return dataset[indices], [dataset_target[i:i+targetSize]]
+            data.append(dfScaled[indices])
+            labels.append([dataset_target[i:i+targetSize]])
+        return data, labels
 
     def __writeListFile(self,file,symbolName):
-        mode = 'a' if os.path.exists(self.discardedCompaniesList ) else 'w'
-        with open(self.discardedCompaniesList , mode) as outfile:
-            outfile.write(file.split('_')[0] + '\n')
+        mode = 'a' if os.path.exists(file ) else 'w'
+        with open(file , mode) as outfile:
+            outfile.write(symbolName + '\n')
 
     def __findSplitIndex(self,dfLength):
         splitIndex = int(dfLength*self.splitRatio) - dfLength%self.targetLength
-        return splitIndex
+        return splitIndex+1
 
     def __getLabels(self,df):
         columnLabels = df.columns.values
-        self.inputFeatures = [self.targets] + [x for x in columnLabels if x not in self.targets]
+        self.inputFeatures = self.targets + [x for x in columnLabels if x not in self.targets]
